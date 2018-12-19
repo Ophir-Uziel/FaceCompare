a5c05cb1b8f7107963ce67eb606f3b4825ea0003
ophiru98@gmail.com <ophiru98@gmail.com>
2018-12-19 14:35:18 +0200

    a

--- trash/resize.py
+++
@@ -1,5 +0,0 @@
-import cv2
-def resize(image, max_height, max_width):
-    current_size = image.size()
-    factor = min(max_height/current_size[0], max_width/current_size[1])
-    return cv2.resize(image, (0,0), factor, factor, cv2.INTER_AREA)
--- trash/try_encoding.py
+++
@@ -1,7 +0,0 @@
-import face_recognition
-import cv2
-
-img = cv2.imread('IlhamAnas.jpg')
-a = face_recognition.face_encodings(img)
-
-print(a[0])
